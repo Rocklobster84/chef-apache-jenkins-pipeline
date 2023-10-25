2681e1e53acd4fcdfe4525eebdc63c7ec72befec
Stephanie Thompson <thompson.stephanie84@gmail.com>
2023-10-25 18:37:39 +0000

    Removed = signs from config

--- config.rb
+++ config.rb
@@ -1,9 +1,15 @@
-current_dir = File.dirname(__FILE__)
-node_name         'student01'
-client_key      = '#{current_dir}/student01.pem'
+=begin
+[default]
+client_name = 'student01'
+client_key = '/home/ubuntu/.chef/student01.pem'
 chef_server_url = 'https://ip-172-31-62-128.ec2.internal/organizations/student01'
-ssl_verify_mode ':verify_none'
+=end
 
+current_dir = File.dirname(__FILE__)
+node_name       'student01'
+client_key      '#{current_dir}/student01.pem'
+chef_server_url 'https://ip-172-31-62-128.ec2.internal/organizations/student01'
+ssl_verify_mode ':verify_none'
 
 =begin
 current_dir = File.dirname(__FILE__)
